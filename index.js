715abc9955fc6996dd98bf1518040e5cfaf4bb8e
Jimmy <jdicolandrea@gmail.com>
2017-07-24 21:49:32 -0400

    create mvp

---
+++ index.js
@@ -0,0 +1,10 @@
+(function() {
+  const navbar = document.querySelector('#navbar');
+  let topOfNav = navbar.offsetTop;
+
+  window.scroll({
+    behavior: 'smooth'
+  })
+
+  console.log(navbar);
+})();
